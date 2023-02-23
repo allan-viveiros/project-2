558b01c0e1fdd36339aecae46be4b03b6d7f7568
allan-viveiros <viveirosallan@gmail.com>
2023-02-23 17:08:35 -0500

    pseudo code

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// Pseudo Code
+
+// Step 1 -> Create a landing page with the heading and title "Amazing Trip" and a <p> explain about the page.
+// Step 2 -> create a form to receive the user inputs "name, email, Country visited, comments, avatar preset img" 
+// Step 3 -> Create a div to receive the user's input
+// Step 4 -> Add event listener for the form submit button, to get the inputs and shows on the proper div 
+// Step 5 -> Declare a variable for each user's input.
