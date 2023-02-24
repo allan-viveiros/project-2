91893d0558a2ffae19c8d40499d02c1582e9c34d
adrienpolselli <adrienpolselli@hotmail.com>
2023-02-24 11:47:14 -0500

    Updating Pseudo code

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 import app from "./firebase";
-
 //Import the sdk for the rtd product
 //import the getDatabse and ref nnames export fromt the RTD module
 import {
@@ -17,14 +16,42 @@ import {
 
 
 
+// PSEUDO CODE
+
+//FIREBASE SETUP
+        // Step 1 -> Create a file (firebase.js) to configure and export the Firebase object.
+        // Step 2 -> Import the database object, and any required Firebase modules at the top of the main app file (app.js)
+
+//HTML document:
+        // Step 1 -> Create a landing page with the heading and title "Amazing Trip" and a <p> explain about the page.
+        // Step 2 -> create a form to receive the user inputs "name, email, Country visited, comments, avatar preset img" 
+        // Step 3 -> Create a section to receive the user's input in a UL
 
 
+//JAVASCRIPT:
 
+        // Step 1 -> Use document.querySelector() to get all the JS objects:
+             // One that points to the UL where the user card will be displayed.
+             // One that points to the text input where users will add his user name.
+             // One that points to the text input where users will add his email.
+             // One that points to the select input where users will add his choice of country.
+             // One that points to the text area  where users will add his comment.
+             // One that points to the form containing all those above data.
 
-// Pseudo Code
+        // Step 2 -> Call getDatabase() and ref() to create a reference to the Firebase database.
 
-// Step 1 -> Create a landing page with the heading and title "Amazing Trip" and a <p> explain about the page.
-// Step 2 -> create a form to receive the user inputs "name, email, Country visited, comments, avatar preset img" 
-// Step 3 -> Create a div to receive the user's input
-// Step 4 -> Add event listener for the form submit button, to get the inputs and shows on the proper div 
-// Step 5 -> Declare a variable for each user's input.
+        // Step 3 -> Call onValue() to get a snapshot of the database, and to get a new snapshot any time the data changes/updates.
+            // Clear all content in the UL on the page, so that we can update it with the current list of user's cards.
+
+        // Step 4 -> Loop through the users object.
+            // For each new user in the database:
+                // Create a new LI with:
+                // A div that contain the user avatar,a <h2> with the country selected, a <p> with the user name, another <p> with the user email and a last <p> with the user comment. Finally, we'll do a <p> to display date and hour of the comment.
+                // .append() the new LI into the UL on the page.
+
+        // Step 5 -> Add event listener for the form submit button, to get the inputs and shows on the proper section 
+             // Prevent the submit from causing the page to refresh 
+            // Get what the user wrote in the all inputs.
+            // Use Firebase's get() function to get a snapshot of current list of user's card in the database.
+             // Use Firebase's update() function to send the updated list of user's card to Firebase.
+
